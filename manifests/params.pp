204f3c87ebb1a8ce342c2bba3668562ff2314251
Guzmán Brasó <guzman.braso@gmail.com>
2017-02-23 18:23:36 -0300

    Shitty shitty shitty module

--- manifests/params.pp
+++ manifests/params.pp
@@ -7,7 +7,7 @@ class liquibase::params {
   $h2_version         = '1.4.191'
   $hsqldb_version     = '2.3.4'
   $jtds_version       = '1.3.1'
-  $mysql_version      = '6.0.2'
+  $mysql_version      = '5.1.40'
   $sqlite_version     = '3.7.8'
   $jt400_version      = '9.0'
   $mariadb_version    = '1.4.5'
